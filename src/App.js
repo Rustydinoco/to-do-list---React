5e124c1a45ae40d99c16f253029a8848fc2ed10a
Bilal Achmad Ramadhan <bilaldoank04@gmail.com>
2024-01-24 12:27:39 +0700

    To do list /React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+
+const ListItem = [
+  {
+    id: 1,
+    title: "makan",
+    done: true,
+  },
+  {
+    id: 2,
+    title: "minum",
+    done: false,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <Checklist />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <span className="logo">Gocek</span>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>Tambahkan Task Kamu</h3>
+    </div>
+  );
+}
+
+function Checklist() {
+  return (
+    <div className="list">
+      <ul>
+        {ListItem.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" name="" id="" />
+      <span style={{ textDecoration: item.done ? "line-through" : "" }}>
+        {item.title}
+      </span>
+      <button>X</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <span>Kamun punya X catatan dan X yang belum diselesaikan (X%)</span>
+    </footer>
   );
 }
 
